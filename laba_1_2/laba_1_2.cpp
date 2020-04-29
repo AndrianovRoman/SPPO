68ada64889bbfb8ec586ff93b5f0db93a1a54a03
r_andrianov02@mail.ru <r_andrianov02@mail.ru>
2020-04-29 17:28:30 +0300

    Добавьте файлы проекта.

---
+++ laba_1_2/laba_1_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+double f1(double m) {
+	double v;
+	if (m >= 0)
+		v = m * m;
+	else {
+		v = abs(m);
+	}
+	return v;
+}
+double f2(double& X)
+{
+	X = f1(X);
+	return X;
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int y, b;
+	double x, result;
+	cout << "Введите первое значение ";
+	cin >> x;
+	cout << "Введите количество ";
+	cin >> y;
+	cout << "Введите шаг ";
+	cin >> b;
+
+	for (int i = 1; i <= y; i++, x += b) {
+		cout << "Результат " << f2(x) << "\n";
+	}
+
+	system("pause");
+	return 0;
+}
